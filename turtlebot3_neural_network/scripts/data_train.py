6ec5a6a4b43a2eafe74baf8193e97c72bda8a75d
jerin-joy <jerinmemana@gmail.com>
2023-02-24 16:57:02 +0100

    split train and test datasets for comparison

--- turtlebot3_neural_network/scripts/data_train.py
+++ turtlebot3_neural_network/scripts/data_train.py
@@ -8,6 +8,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 # Read Data
 columns = []
@@ -31,36 +32,43 @@ print(f"Number of Data Entries: {len(df)}")
 print(f"Number of v Entries: {len(df[df['v'] == 1])}")
 print(f"Number of w Entries: {len(df[df['w'] == 1])}")
 
-y = pd.concat([df.pop(x) for x in ['v', 'w']], axis=1)
-X = df
+# y = pd.concat([df.pop(x) for x in ['v', 'w']], axis=1)
+# X = df
+
+# Split into training and test sets
+X = df.drop(['v', 'w'], axis=1)
+y = df[['v', 'w']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 print("Prepared Data")
-print("y Dataframe\n")
-print(y.head(5))
+print("y_train Dataframe\n")
+print(y_train.head(5))
 
-print("X Dataframe\n")
-print(X.head(5))
+print("X_train Dataframe\n")
+print(X_train.head(5))
 
 # Decision Tree Classifier
 dt_model = DecisionTreeClassifier()
-dt_model.fit(X, y)
+dt_model.fit(X_train, y_train)
 
 # Random Forest Classifier
 rf_model = RandomForestClassifier()
-rf_model.fit(X, y)
+rf_model.fit(X_train, y_train)
 
 # K-Nearest Neighbors Classifier
 knn_model = KNeighborsClassifier()
-knn_model.fit(X, y)
+knn_model.fit(X_train, y_train)
 
 # Tensorflow Neural Network Classifier
-X_tensor = tf.convert_to_tensor(X)
+X_tensor = tf.convert_to_tensor(X_train)
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(100, activation='relu'),
     tf.keras.layers.Dense(2, activation='sigmoid')
 ])
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_tensor, y, epochs=30, batch_size=8, validation_split=0.2, shuffle=True)
+model.fit(X_tensor, y_train)
 
 # Save all models
 joblib.dump(rf_model, 'model_random_forest.joblib')
